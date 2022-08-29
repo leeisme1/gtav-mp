5e66826ee4ed3a6aa4ce49ba31bdcf277f4be115
jonhien <108387796+jonhien1@users.noreply.github.com>
2022-08-29 20:55:48 +0700

    Update startup.ts

--- src/core/server/startup.ts
+++ src/core/server/startup.ts
@@ -1,12 +1,12 @@
 import * as alt from 'alt-server';
 
-alt.log(`alt:V Server - Boilerplate Started`);
+alt.log(`alt:V Server - Started`);
 alt.on('playerConnect', handlePlayerConnect);
 
 function handlePlayerConnect(player: alt.Player) {
-    alt.log(`[${player.id}] ${player.name} has connected to the server.`);
+    alt.log(`[Player Connection] ${player.name} (ID: ${player.id}) đã truy cập vào máy chủ GTA:V Multiplayer.`);
 
-    player.model = 'mp_m_freemode_01';
+    player.model = 'u_m_y_abner';
     player.spawn(36.19486618041992, 859.3850708007812, 197.71343994140625, 0);
-    alt.emitClient(player, 'log:Console', 'alt:V Server - Boilerplate Started');
+    alt.emitClient(player, 'log:Console', 'alt:V Server - #1');
 }
